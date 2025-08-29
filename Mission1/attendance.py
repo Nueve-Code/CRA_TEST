f67eb3231a27ad8854915423bdf868bdea7dca47
Nueve-Code <rnska96@gmail.com>
2025-08-29 09:50:38 +0900

    refactor special day parameters

--- Mission1/attendance.py
+++ Mission1/attendance.py
@@ -1,16 +1,18 @@
 member_ids = {}
 total_members = 0
 
+MAX_MEMBER_COUNT = 100
+
 # dat[사용자ID][요일]
-attendance_data = [[0] * 100 for _ in range(100)]
-points = [0] * 100
-grade = [0] * 100
-names = [''] * 100
-wed = [0] * 100
-weeken = [0] * 100
+attendance_data = [[0] * MAX_MEMBER_COUNT for _ in range(MAX_MEMBER_COUNT)]
+points = [0] * MAX_MEMBER_COUNT
+grade = [0] * MAX_MEMBER_COUNT
+names = [''] * MAX_MEMBER_COUNT
+wednesday_attendance = [0] * MAX_MEMBER_COUNT
+weekend_attendance = [0] * MAX_MEMBER_COUNT
 
 
-def init_members(member_name, day):
+def init_members(member_name, day: str):
     global total_members
 
     if member_name not in member_ids:
@@ -20,15 +22,14 @@ def init_members(member_name, day):
 
     member_id = member_ids[member_name]
 
-    day_idx = get_day_idx(day)
     add_point = get_points_per_day(day)
-    add_additional_points(day, member_id)
+    check_special_day_attendance(day, member_id)
 
-    attendance_data[member_id][day_idx] += 1
+    attendance_data[member_id][get_day_idx(day)] += 1
     points[member_id] += add_point
 
 
-def get_day_idx(day):
+def get_day_idx(day: str):
     day_dict = {
         'monday': 0,
         'tuesday': 1,
@@ -42,16 +43,16 @@ def get_day_idx(day):
     return day_dict[day]
 
 
-def add_additional_points(day, member_id):
+def check_special_day_attendance(day: str, member_id):
     if day == "wednesday":
-        wed[member_id] += 1
+        wednesday_attendance[member_id] += 1
     elif day == "saturday":
-        weeken[member_id] += 1
+        weekend_attendance[member_id] += 1
     elif day == "sunday":
-        weeken[member_id] += 1
+        weekend_attendance[member_id] += 1
 
 
-def get_points_per_day(day):
+def get_points_per_day(day: str):
     point_per_day = {
         'monday': 1,
         'tuesday': 1,
@@ -77,10 +78,7 @@ def input_file():
                     init_members(parts[0], parts[1])
 
         for i in range(1, total_members + 1):
-            if attendance_data[i][2] > 9:
-                points[i] += 10
-            if attendance_data[i][5] + attendance_data[i][6] > 9:
-                points[i] += 10
+            points[i] += get_additional_points(i)
 
             if points[i] >= 50:
                 grade[i] = 1
@@ -100,11 +98,21 @@ def input_file():
         print("\nRemoved player")
         print("==============")
         for i in range(1, total_members + 1):
-            if grade[i] not in (1, 2) and wed[i] == 0 and weeken[i] == 0:
+            if grade[i] not in (1, 2) and wednesday_attendance[i] == 0 and weekend_attendance[i] == 0:
                 print(names[i])
 
     except FileNotFoundError:
         print("파일을 찾을 수 없습니다.")
+
+
+def get_additional_points(i):
+    wednesday_attendance_count = attendance_data[i][get_day_idx('wednesday')]
+    weekend_attendance_count = attendance_data[i][get_day_idx('saturday')] + attendance_data[i][get_day_idx('sunday')]
+
+    if (wednesday_attendance_count > 9) or (weekend_attendance_count > 9) :
+        return 10
+    else:
+        return 0
 
 
 if __name__ == "__main__":
